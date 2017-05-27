3b135e266b00bb13b4d198fd23bcc7ef7c800251
Pil Lip Youn <pillip.youn@gmail.com>
2017-05-27 18:20:11 +0900

    기본적인 model 생성
    - Institute model 생성
    - Course model 생성
    - Course model 생성 시, BASE_DIR/result/ 디렉토리 밑에 folderName으로 디렉토리 자동 생성
    - admin 페이지에서 Institute와 Course 관리 할 수 있도록 설정

--- home/models.py
+++ home/models.py
@@ -3,3 +3,45 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+import os
+
+import sys
+reload(sys)
+sys.setdefaultencoding("utf-8")
+
+class Course(models.Model):
+    title = models.CharField(max_length=100, null=False)
+    lectureNumbers = models.IntegerField(null=False)
+    folderName = models.CharField(max_length=20, null=False)
+
+
+    def __str__(self):
+        return self.title + " lectureNumber : " + str(self.lectureNumbers)
+
+class Institute(models.Model):
+    name = models.CharField(max_length=100, null=False)
+
+    courses = models.ManyToManyField(Course)
+
+    def __str__(self):
+        return self.name
+
+def createDirectory(sender, **kwargs):
+    course = kwargs['instance']
+    directoryName = 'result/' + course.folderName
+
+    absPath = os.path.abspath(directoryName)
+
+    makeDir(absPath)
+
+    print absPath
+
+def makeDir(path):
+    try:
+        os.mkdir(path)
+    except OSError as e:
+        if e.errno == 17:
+            # Dir already exists.
+            pass
+
+models.signals.post_save.connect(createDirectory, sender=Course)
