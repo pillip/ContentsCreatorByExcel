fe3eb4664c3f4da02caab3d2f1da614c8b111007
Pil Lip Youn <pillip.youn@gmail.com>
2017-05-30 20:30:11 +0900

    Add MEDIA_URL and MEDIA_ROOT

--- ContentsCreatorByExcel/settings.py
+++ ContentsCreatorByExcel/settings.py
@@ -123,6 +123,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 MEDIA_URL = '/media/'
-MEDAI_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
